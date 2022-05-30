b794209181022a0cd4f11979aedab35fec82752b
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-05-31 01:48:10 +0530

    check whether a number is palindrome or not

---
+++ check_number_palindrome_or_not.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n, reversed = 0, remainder, original;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    original = n;
+
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+    if (original == reversed)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
+
+    return 0;
+}
+//
+//
+// Output ==>
+// Enter an integer: 565
+// 565 is a palindrome.
