e5d6928619a6db91658c608d1b2fa25b95189bdc
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-05-31 02:15:40 +0530

    check whether a number is odd or even

---
+++ check_odd_or_even.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter any number to check even or odd: ");
+    scanf("%d", &num);
+    (num % 2 == 0) ? printf("The number is EVEN") : printf("The number is ODD");
+
+    return 0;
+}
+//
+//
+// Output ==>
+// Enter any number to check even or odd: 4
+// The number is EVEN
