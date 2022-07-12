b5cfc840a552990b171d7ea7bf1d0fde5a078c3e
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-07-12 18:04:21 +0530

    Bubble sort implementation

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -3,28 +3,28 @@
 int main()
 {
 
-    int arr[50], n, x, y, temp;
+    int arr[50], n, i, j, temp;
     printf("Enter Number of Elements you want in array: ");
     scanf("%d", &n);
     printf("Please Enter the Value of Elements: ");
-    for (x = 0; x < n; x++)
-        scanf("%d", &arr[x]);
-    for (x = 0; x < n - 1; x++)
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    for (i = 0; i < n - 1; i++)
     {
-        for (y = 0; y < n - x - 1; y++)
+        for (j = 0; j < n - i - 1; j++)
         {
-            if (arr[y] > arr[y + 1])
+            if (arr[j] > arr[j + 1])
             {
-                temp = arr[y];
-                arr[y] = arr[y + 1];
-                arr[y + 1] = temp;
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
     printf("Array after implementing bubble sort: ");
-    for (x = 0; x < n; x++)
+    for (i = 0; i < n; i++)
     {
-        printf("%d  ", arr[x]);
+        printf("%d  ", arr[i]);
     }
 }
 //
